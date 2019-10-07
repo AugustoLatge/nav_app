8d237e536dde0f70bb381c87c0fdd1cf83300e0a
Augusto <augustoslatge@gmail.com>
2019-10-07 16:43:50 -0400

    nav app files

---
+++ nav-app/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import { useScreens } from 'react-native-screens';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+
+import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
+
+useScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)}/>;
+  }
+
+  return <Provider store={store}><MealsNavigator /></Provider>;
+};
